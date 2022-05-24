7ef7f0bd9a13b13f54badecb74945e615e9ace9f
LMN1590 <minhnghiale1590@gmail.com>
2022-05-24 15:23:13 +0700

    update reading from excel files without modification

--- main2.py
+++ main2.py
@@ -9,11 +9,12 @@ import pandas as pd
 
 def main():
     df=pd.read_excel('trainingset.xls')
-    df=pd.DataFrame(df,columns=['A','D'])
+    df=pd.DataFrame(df,columns=['A','B','C'])
     dict={}
     df = df.reset_index()  # make sure indexes pair with number of rows
     for index, row in df.iterrows():
-        dict[row['A']]=row['D']
+        dict[row['A']]=row['B']/(row['B']+row['C'])
+
     pygame.init() 
     while True:
         #Setting up UI
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 df=pd.read_excel('trainingset.xls')
-df=pd.DataFrame(df,columns=['A','D'])
+df=pd.DataFrame(df,columns=['A','B','C'])
 dict={}
 df = df.reset_index()  # make sure indexes pair with number of rows
 for index, row in df.iterrows():
-    dict[row['A']]=row['D']
-print(dict[472861318643])
+    dict[row['A']]=row['B']/(row['B']+row['C'])
+print(dict)
